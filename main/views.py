02ec8471590cb9ce22b1422d3455e9c12d21c61f
kimgaram <kimgarm0718@naver.com>
2025-04-30 17:07:23 +0900

    250430_v1_[1]새 브랜치 생성 테스트 위해 주석만 등록함

--- main/views.py
+++ main/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import JsonResponse
 
 # Create your views here.
+#250430_main 외 새 브랜치 생성 후 
+# 그 브랜치에서 작업하기 테스트 중입니다
 def main(request):
     return render(request, 'main.html')
